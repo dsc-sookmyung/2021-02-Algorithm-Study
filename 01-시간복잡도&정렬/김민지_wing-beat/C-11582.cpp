be9ff6f71693dc6882291472eafdcc08abb9f7ce
wing-beat <wingbeat1115@gmail.com>
2021-10-04 00:24:24 +0900

    김민지 1주차 과제 제출

--- 01-시간복잡도&정렬/김민지_wing-beat/C-11582.cpp
+++ 01-시간복잡도&정렬/김민지_wing-beat/C-11582.cpp
@@ -1,34 +1,26 @@
 #include <iostream>
-using namespace std;
 #define MAX_SIZE 1048576
+using namespace std;
 int sorted[MAX_SIZE];
 int n, person;
 
 void merge(int list[], int left, int mid, int right){
-    if( (right-left) > (n/person) ) return;
+    if((right-left) > (n/person)) return;
     
-    int i, j, k;
-    i = left;
-    j = mid+1;
-    k = 0;
+    int i = left, j = mid+1, k = 0;
     
-    /* 분할 정렬된 list의 합병 */
     while(i<=mid && j<=right){
         if(list[i]<=list[j])
           sorted[k++] = list[i++];
         else
           sorted[k++] = list[j++];
     }
-    
-    // 남아 있는 값들을 일괄 복사
     while(i <= mid){ 
         sorted[k++] = list[i++];
     }
-    // 남아 있는 값들을 일괄 복사
     while (j <= right) {
         sorted[k++] = list[j++];
     }
-    // 배열 sorted[](임시 배열)의 리스트를 배열 list[]로 재복사
     for(int l=left; l<=right; l++){
         list[l] = sorted[l-left];
     }
@@ -37,9 +29,9 @@ void merge(int list[], int left, int mid, int right){
 void merge_sort(int list[], int left, int right){
     if(left == right) return;
     int mid = (left+right)/2; 
-    merge_sort(list, left, mid); // 앞쪽 부분 리스트 정렬 -정복(Conquer)
-    merge_sort(list, mid+1, right); // 뒤쪽 부분 리스트 정렬 -정복(Conquer)
-    merge(list, left, mid, right); // 정렬된 2개의 부분 배열을 합병하는 과정 -결합(Combine)
+    merge_sort(list, left, mid); 
+    merge_sort(list, mid+1, right);
+    merge(list, left, mid, right); 
 }
 
 int main(){
@@ -53,10 +45,8 @@ int main(){
     }
     cin >> person;
     
-    // 합병 정렬 수행(left: 배열의 시작 = 0, right: 배열의 끝 = 7)
     merge_sort(list, 0, n-1);
     
-    // 정렬 결과 출력
     for(int i=0; i<n; i++){
         cout << list[i] << ' ';
     }
