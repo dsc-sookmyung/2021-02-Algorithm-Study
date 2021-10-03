9a2bf63c17faa0345d08703f0b6c52d9bd3dc9bb
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2021-10-03 21:18:21 +0900

    강수아 1주차 과제 제출

--- 01-시간복잡도&정렬/강수아_rkdtndk99/B-10610.cpp
+++ 01-시간복잡도&정렬/강수아_rkdtndk99/B-10610.cpp
@@ -1,4 +1,35 @@
-//
-// Created by 강수아 on 2021-10-02.
-//
+//30
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    string str;
+    cin >> str;
+    vector<int> v;
+
+    //더해서 3의 배수인지 보기
+    int total =0;
+    for (int i=0; i<str.size();i++){
+        total += str[i] - '0';
+        v.push_back(str[i]-'0');
+    }
+
+    sort(v.begin(), v.end(), greater<int>());
+
+    if(total%3==0 && v[str.size()-1]==0){
+        for(auto i : v)
+            cout << i;
+    } else{
+        cout << -1;
+    }
+}
+
 
